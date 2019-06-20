52ab65b2f94223bd0ed3c6935ad37615ad83024b
Kesha Antonov <innokenty.longway@gmail.com>
2019-06-20 11:44:37 +0300

    Bugfixes: bug found by TG. When you open a chat, all the elements are loaded one after another: chat name > the latest message > profile pictures. Consecutive display of elements is OK if it is intended to improve speed perception.  However, some elements (e.g. profile pictures) blink, which is not OK.

--- src/Components/Tile/UserTileControl.js
+++ src/Components/Tile/UserTileControl.js
@@ -5,156 +5,160 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { getUserLetters } from '../../Utils/User';
-import { loadChatContent } from '../../Utils/File';
-import UserStore from '../../Stores/UserStore';
-import ChatStore from '../../Stores/ChatStore';
-import FileStore from '../../Stores/FileStore';
-import './UserTileControl.css';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { getUserLetters } from '../../Utils/User'
+import { loadChatContent } from '../../Utils/File'
+import UserStore from '../../Stores/UserStore'
+import ChatStore from '../../Stores/ChatStore'
+import FileStore from '../../Stores/FileStore'
+import './UserTileControl.css'
 
 class UserTileControl extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         if (nextProps.userId !== this.props.userId) {
-            return true;
+            return true
         }
 
-        return false;
+        return false
     }
 
     componentDidMount() {
-        FileStore.on('clientUpdateUserBlob', this.onClientUpdateUserBlob);
-        FileStore.on('clientUpdateChatBlob', this.onClientUpdateChatBlob);
-        ChatStore.on('updateChatPhoto', this.onUpdateChatPhoto);
-        ChatStore.on('updateChatTitle', this.onUpdateChatTitle);
+        FileStore.on('clientUpdateUserBlob', this.onClientUpdateUserBlob)
+        FileStore.on('clientUpdateChatBlob', this.onClientUpdateChatBlob)
+        ChatStore.on('updateChatPhoto', this.onUpdateChatPhoto)
+        ChatStore.on('updateChatTitle', this.onUpdateChatTitle)
     }
 
     componentWillUnmount() {
-        FileStore.removeListener('clientUpdateUserBlob', this.onClientUpdateUserBlob);
-        FileStore.removeListener('clientUpdateChatBlob', this.onClientUpdateChatBlob);
-        ChatStore.removeListener('updateChatPhoto', this.onUpdateChatPhoto);
-        ChatStore.removeListener('updateChatTitle', this.onUpdateChatTitle);
+        FileStore.removeListener('clientUpdateUserBlob', this.onClientUpdateUserBlob)
+        FileStore.removeListener('clientUpdateChatBlob', this.onClientUpdateChatBlob)
+        ChatStore.removeListener('updateChatPhoto', this.onUpdateChatPhoto)
+        ChatStore.removeListener('updateChatTitle', this.onUpdateChatTitle)
     }
 
     onClientUpdateUserBlob = update => {
-        const { userId } = this.props;
+        const { userId } = this.props
 
         if (userId === update.userId) {
-            this.forceUpdate();
+            this.forceUpdate()
         }
-    };
+    }
 
     onClientUpdateChatBlob = update => {
-        const { userId } = this.props;
+        const { userId } = this.props
 
-        const chat = ChatStore.get(update.chatId);
-        if (!chat) return;
-        if (!chat.type) return;
+        const chat = ChatStore.get(update.chatId)
+        if (!chat) return
+        if (!chat.type) return
 
         switch (chat.type['@type']) {
             case 'chatTypeBasicGroup':
             case 'chatTypeSupergroup': {
-                return;
+                return
             }
             case 'chatTypePrivate':
             case 'chatTypeSecret': {
                 if (chat.type.user_id === userId) {
                     //console.log('UserTileControl.onClientUpdateChatBlob user_id=' + userId);
-                    this.forceUpdate();
+                    this.forceUpdate()
                 }
             }
         }
-    };
+    }
 
     onUpdateChatPhoto = update => {
-        const { userId } = this.props;
+        const { userId } = this.props
 
-        const chat = ChatStore.get(update.chat_id);
-        if (!chat) return;
-        if (!chat.type) return;
+        const chat = ChatStore.get(update.chat_id)
+        if (!chat) return
+        if (!chat.type) return
 
         switch (chat.type['@type']) {
             case 'chatTypeBasicGroup':
             case 'chatTypeSupergroup': {
-                return;
+                return
             }
             case 'chatTypePrivate':
             case 'chatTypeSecret': {
                 if (chat.type.user_id === userId) {
                     if (!update.photo) {
                         //console.log('UserTileControl.onUpdateChatPhoto user_id=' + userId);
-                        this.forceUpdate();
+                        this.forceUpdate()
                     } else {
-                        const store = FileStore.getStore();
-                        loadChatContent(store, chat);
+                        const store = FileStore.getStore()
+                        loadChatContent(store, chat)
                     }
                 }
             }
         }
-    };
+    }
 
     onUpdateChatTitle = update => {
-        const { userId } = this.props;
+        const { userId } = this.props
 
-        const chat = ChatStore.get(update.chat_id);
-        if (!chat) return;
-        if (!chat.type) return;
+        const chat = ChatStore.get(update.chat_id)
+        if (!chat) return
+        if (!chat.type) return
 
         switch (chat.type['@type']) {
             case 'chatTypeBasicGroup':
             case 'chatTypeSupergroup': {
-                return;
+                return
             }
             case 'chatTypePrivate':
             case 'chatTypeSecret': {
                 if (chat.type.user_id === userId && !chat.photo) {
                     //console.log('UserTileControl.onUpdateChatTitle user_id=' + userId);
-                    this.forceUpdate();
+                    this.forceUpdate()
                 }
             }
         }
-    };
+    }
 
     handleSelect = event => {
-        const { userId, onSelect } = this.props;
-        if (!onSelect) return;
+        const { userId, onSelect } = this.props
+        if (!onSelect) return
 
-        event.stopPropagation();
-        onSelect(userId);
-    };
+        event.stopPropagation()
+        onSelect(userId)
+    }
 
     render() {
-        const { userId, onSelect } = this.props;
-        let { user } = this.props;
-        if (!userId && !user) return null;
+        const { userId, onSelect } = this.props
+        let { user } = this.props
+        if (!userId && !user) return null
 
-        user = UserStore.get(userId) || user;
-        if (!user) return null;
+        user = UserStore.get(userId) || user
+        if (!user) return null
 
-        const { profile_photo } = user;
+        const { profile_photo } = user
 
-        const letters = getUserLetters(user);
-        const blob = profile_photo && profile_photo.small ? FileStore.getBlob(profile_photo.small.id) : null;
-        const src = FileStore.getBlobUrl(blob);
-        const tileColor = `tile_color_${(Math.abs(userId) % 8) + 1}`;
-        const className = classNames('tile-photo', { [tileColor]: !blob }, { pointer: onSelect });
+        const letters = getUserLetters(user)
+        const blob =
+            profile_photo && profile_photo.small ? FileStore.getBlob(profile_photo.small.id) : null
+        const src = FileStore.getBlobUrl(blob)
+        const tileColor = `tile_color_${(Math.abs(userId) % 8) + 1}`
+        const className = classNames('tile-photo', { [tileColor]: !blob }, { pointer: onSelect })
 
-        return src ? (
-            <img className={className} src={src} draggable={false} alt='' onClick={this.handleSelect} />
-        ) : (
+        return (
             <div className={className} onClick={this.handleSelect}>
                 <span className='tile-text'>{letters}</span>
+                {src && (
+                    <div className={'tile-photo-image'}>
+                        <img className={className} src={src} draggable={false} alt='' />
+                    </div>
+                )}
             </div>
-        );
+        )
     }
 }
 
 UserTileControl.propTypes = {
     userId: PropTypes.number.isRequired,
     user: PropTypes.object,
-    onSelect: PropTypes.func
-};
+    onSelect: PropTypes.func,
+}
 
-export default UserTileControl;
+export default UserTileControl
