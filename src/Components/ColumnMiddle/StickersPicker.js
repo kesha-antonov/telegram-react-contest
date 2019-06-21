c5db5a346361f3b8dd0a2ade6bf0a565a0ebd826
Kesha Antonov <innokenty.longway@gmail.com>
2019-06-21 14:05:53 +0300

    Stickers picker does not have scrollbars. | Done: added styled scrollbar.

--- src/Components/ColumnMiddle/StickersPicker.js
+++ src/Components/ColumnMiddle/StickersPicker.js
@@ -5,350 +5,360 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import StickerSet from './StickerSet';
-import StickersPickerHeader from './StickersPickerHeader';
-import { debounce, throttle } from '../../Utils/Common';
-import { loadStickerContent, loadStickerSetContent } from '../../Utils/File';
-import { getNeighborStickersFromSets, getStickers } from '../../Utils/Media';
-import FileStore from '../../Stores/FileStore';
-import TdLibController from '../../Controllers/TdLibController';
-import './StickersPicker.css';
+import React from 'react'
+import * as ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import StickerSet from './StickerSet'
+import StickersPickerHeader from './StickersPickerHeader'
+import { debounce, throttle } from '../../Utils/Common'
+import { loadStickerContent, loadStickerSetContent } from '../../Utils/File'
+import { getNeighborStickersFromSets, getStickers } from '../../Utils/Media'
+import FileStore from '../../Stores/FileStore'
+import TdLibController from '../../Controllers/TdLibController'
+import Scrollbar from '../Scrollbar'
+import './StickersPicker.css'
 
 class StickersPicker extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
 
-        this.scrollRef = React.createRef();
-        this.itemsMap = new Map();
-        this.loadedSets = new Map();
+        this.scrollRef = null
+        this.itemsMap = new Map()
+        this.loadedSets = new Map()
 
         this.state = {
             stickerSets: null,
             sets: [],
             headerStickers: [],
-            position: 0
-        };
+            position: 0,
+        }
 
-        this.loadInViewContentOnScrollEnd = debounce(this.loadInViewContentOnScrollEnd, 100);
-        this.loadInViewContentOnScroll = throttle(this.loadInViewContentOnScroll, 2000);
-        this.updatePosition = throttle(this.updatePosition, 250);
+        this.loadInViewContentOnScrollEnd = debounce(this.loadInViewContentOnScrollEnd, 100)
+        this.loadInViewContentOnScroll = throttle(this.loadInViewContentOnScroll, 2000)
+        this.updatePosition = throttle(this.updatePosition, 250)
     }
 
     shouldComponentUpdate(nextProps, nextState, nextContext) {
-        const { position, stickerSets, sets, showPreview } = this.state;
+        const { position, stickerSets, sets, showPreview } = this.state
 
         if (nextState.stickerSets !== stickerSets) {
-            return true;
+            return true
         }
 
         if (nextState.sets !== sets) {
-            return true;
+            return true
         }
 
         if (nextState.showPreview !== showPreview) {
-            return true;
+            return true
         }
 
         if (nextState.position !== position) {
-            return true;
+            return true
         }
 
-        return false;
+        return false
     }
 
     scrollTop = () => {
-        this.scrollRef.current.scrollTop = 0;
-    };
+        this.scrollRef.scrollTop = 0
+    }
 
     loadContent = async (stickerSets, sets) => {
-        if (this.state.stickerSets) return;
+        if (this.state.stickerSets) return
 
         if (!sets) {
             const result = await TdLibController.send({
                 '@type': 'getInstalledStickerSets',
-                is_masks: false
-            });
+                is_masks: false,
+            })
 
-            const promises = [];
+            const promises = []
             result.sets.forEach(x => {
                 promises.push(
                     TdLibController.send({
                         '@type': 'getStickerSet',
-                        set_id: x.id
+                        set_id: x.id,
                     })
-                );
-            });
+                )
+            })
 
-            sets = await Promise.all(promises);
+            sets = await Promise.all(promises)
         }
 
-        const slicedSets = sets.slice(0, 5);
+        const slicedSets = sets.slice(0, 5)
         const headerStickers = sets.reduce((preview, set) => {
             if (set.stickers.length > 0) {
-                preview.push(set.stickers[0]);
+                preview.push(set.stickers[0])
             }
-            return preview;
-        }, []);
+            return preview
+        }, [])
         this.setState({
             stickerSets,
             sets: slicedSets,
-            headerStickers
-        });
-        this.setsLength = slicedSets.length;
-    };
+            headerStickers,
+        })
+        this.setsLength = slicedSets.length
+    }
 
     loadInViewContentOnScroll = () => {
-        this.loadInViewContent();
-    };
+        this.loadInViewContent()
+    }
 
     loadInViewContentOnScrollEnd = () => {
-        this.loadInViewContent(400);
-    };
+        this.loadInViewContent(400)
+    }
 
     loadInViewContent = (padding = 0) => {
-        const scroll = this.scrollRef.current;
+        const scroll = this.scrollRef
 
-        const { sets } = this.state;
+        const { sets } = this.state
 
-        const inViewItems = [];
+        const inViewItems = []
         sets.forEach(x => {
-            const item = this.itemsMap.get(x.id);
-            const node = ReactDOM.findDOMNode(item);
+            const item = this.itemsMap.get(x.id)
+            const node = ReactDOM.findDOMNode(item)
             if (node) {
-                const topBorder = scroll.scrollTop - padding;
-                const bottomBorder = scroll.scrollTop + scroll.offsetHeight + padding;
+                const topBorder = scroll.scrollTop - padding
+                const bottomBorder = scroll.scrollTop + scroll.offsetHeight + padding
 
-                const nodeTop = node.offsetTop;
-                const nodeBottom = node.offsetTop + node.clientHeight;
+                const nodeTop = node.offsetTop
+                const nodeBottom = node.offsetTop + node.clientHeight
 
                 if (nodeTop >= topBorder && node.offsetTop <= bottomBorder) {
-                    inViewItems.push(x);
+                    inViewItems.push(x)
                 } else if (nodeBottom >= topBorder && nodeBottom <= bottomBorder) {
-                    inViewItems.push(x);
+                    inViewItems.push(x)
                 } else if (nodeTop <= topBorder && nodeBottom >= bottomBorder) {
-                    inViewItems.push(x);
+                    inViewItems.push(x)
                 }
             }
-        });
+        })
 
         inViewItems.forEach(x => {
-            const store = FileStore.getStore();
+            const store = FileStore.getStore()
             if (!this.loadedSets.has(x.id)) {
-                this.loadedSets.set(x.id, x.id);
-                loadStickerSetContent(store, x);
+                this.loadedSets.set(x.id, x.id)
+                loadStickerSetContent(store, x)
             }
-        });
-    };
+        })
+    }
 
     updatePosition = () => {
-        const scroll = this.scrollRef.current;
+        const scroll = this.scrollRef
 
-        const { sets } = this.state;
-        let minDiff = scroll.scrollHeight;
-        let position = 0;
-        let firstOffsetTop = 0;
+        const { sets } = this.state
+        let minDiff = scroll.scrollHeight
+        let position = 0
+        let firstOffsetTop = 0
         sets.forEach((x, pos) => {
-            const element = this.itemsMap.get(x.id);
+            const element = this.itemsMap.get(x.id)
             if (element) {
-                const node = ReactDOM.findDOMNode(element);
+                const node = ReactDOM.findDOMNode(element)
                 if (node) {
-                    firstOffsetTop = pos === 0 ? node.offsetTop : firstOffsetTop;
+                    firstOffsetTop = pos === 0 ? node.offsetTop : firstOffsetTop
 
-                    const offsetTop = node.offsetTop - firstOffsetTop;
+                    const offsetTop = node.offsetTop - firstOffsetTop
                     if (node && offsetTop <= scroll.scrollTop) {
-                        const diff = Math.abs(scroll.scrollTop - offsetTop);
+                        const diff = Math.abs(scroll.scrollTop - offsetTop)
                         if (diff <= minDiff) {
-                            minDiff = diff;
-                            position = pos;
+                            minDiff = diff
+                            position = pos
                         }
                     }
                 }
             }
-        });
+        })
 
         TdLibController.clientUpdate({
             '@type': 'clientUpdateStickerSetPosition',
-            position
-        });
-        this.setState({ position });
-    };
+            position,
+        })
+        this.setState({ position })
+    }
 
     handleScroll = async () => {
         //this.loadInViewContentOnScroll();
-        this.loadInViewContentOnScrollEnd();
-        this.updatePosition();
+        this.loadInViewContentOnScrollEnd()
+        this.updatePosition()
 
-        const scroll = this.scrollRef.current;
+        const scroll = this.scrollRef
 
-        if (this.loadingChunk) return;
+        if (this.loadingChunk) return
 
-        let loadChunk = false;
+        let loadChunk = false
         if (scroll.scrollTop + scroll.offsetHeight >= scroll.scrollHeight - 400) {
-            loadChunk = true;
+            loadChunk = true
         }
 
         if (!loadChunk) {
-            return false;
+            return false
         }
 
-        const { sets, stickerSets } = this.state;
-        if (stickerSets.sets.length === sets.length) return;
+        const { sets, stickerSets } = this.state
+        if (stickerSets.sets.length === sets.length) return
 
-        this.loadingChunk = true;
-        const promises = [];
+        this.loadingChunk = true
+        const promises = []
         stickerSets.sets.slice(this.setsLength, this.setsLength + 5).forEach(x => {
             promises.push(
                 TdLibController.send({
                     '@type': 'getStickerSet',
-                    set_id: x.id
+                    set_id: x.id,
                 })
-            );
-        });
+            )
+        })
 
-        const result = await Promise.all(promises).finally(() => (this.loadingChunk = false));
+        const result = await Promise.all(promises).finally(() => (this.loadingChunk = false))
 
-        this.setsLength += result.length;
-        let concatSets = sets.concat(result);
-        this.setState({ sets: concatSets });
-    };
+        this.setsLength += result.length
+        let concatSets = sets.concat(result)
+        this.setState({ sets: concatSets })
+    }
 
     loadPreviewContent = stickerId => {
-        const { sets } = this.state;
+        const { sets } = this.state
 
-        const sticker = getStickers(sets).find(x => x.sticker.id === stickerId);
-        if (!sticker) return;
+        const sticker = getStickers(sets).find(x => x.sticker.id === stickerId)
+        if (!sticker) return
 
-        const store = FileStore.getStore();
-        loadStickerContent(store, sticker, null);
+        const store = FileStore.getStore()
+        loadStickerContent(store, sticker, null)
 
-        const stickersPerRow = 5;
-        const preloadStickers = getNeighborStickersFromSets(sticker, sets, stickersPerRow);
+        const stickersPerRow = 5
+        const preloadStickers = getNeighborStickersFromSets(sticker, sets, stickersPerRow)
         preloadStickers.forEach(x => {
-            loadStickerContent(store, x, null);
-        });
-    };
+            loadStickerContent(store, x, null)
+        })
+    }
 
     handleMouseOver = event => {
-        const stickerId = Number(event.target.dataset.stickerId);
-        if (!stickerId) return;
+        const stickerId = Number(event.target.dataset.stickerId)
+        if (!stickerId) return
 
-        if (!this.mouseDown) return;
+        if (!this.mouseDown) return
 
         if (this.mouseDownStickerId !== stickerId) {
-            this.mouseDownStickerId = null;
+            this.mouseDownStickerId = null
         }
-        this.setState({ previewStickerId: stickerId });
-        this.loadPreviewContent(stickerId);
+        this.setState({ previewStickerId: stickerId })
+        this.loadPreviewContent(stickerId)
 
-        const { onPreview } = this.props;
-        const { sets } = this.state;
+        const { onPreview } = this.props
+        const { sets } = this.state
 
-        const sticker = getStickers(sets).find(x => x.sticker.id === stickerId);
-        onPreview(sticker);
-    };
+        const sticker = getStickers(sets).find(x => x.sticker.id === stickerId)
+        onPreview(sticker)
+    }
 
     handleMouseDown = event => {
-        const stickerId = Number(event.target.dataset.stickerId);
-        if (!stickerId) return;
+        const stickerId = Number(event.target.dataset.stickerId)
+        if (!stickerId) return
 
-        this.mouseDownStickerId = stickerId;
-        const now = Date.now();
+        this.mouseDownStickerId = stickerId
+        const now = Date.now()
 
-        this.setState({ previewStickerId: stickerId, timestamp: now, showPreview: false, cancelSend: false });
+        this.setState({
+            previewStickerId: stickerId,
+            timestamp: now,
+            showPreview: false,
+            cancelSend: false,
+        })
         setTimeout(() => {
-            const { timestamp } = this.state;
+            const { timestamp } = this.state
             if (timestamp === now) {
                 this.setState({ showPreview: true, cancelSend: true }, () => {
-                    const { onPreview } = this.props;
-                    const { sets } = this.state;
+                    const { onPreview } = this.props
+                    const { sets } = this.state
 
-                    const sticker = getStickers(sets).find(x => x.sticker.id === stickerId);
-                    onPreview(sticker);
-                });
+                    const sticker = getStickers(sets).find(x => x.sticker.id === stickerId)
+                    onPreview(sticker)
+                })
             }
-        }, 500);
+        }, 500)
 
-        this.loadPreviewContent(stickerId);
+        this.loadPreviewContent(stickerId)
 
-        this.mouseDown = true;
-        document.addEventListener('mouseup', this.handleMouseUp);
+        this.mouseDown = true
+        document.addEventListener('mouseup', this.handleMouseUp)
 
-        event.preventDefault();
-        event.stopPropagation();
-        return false;
-    };
+        event.preventDefault()
+        event.stopPropagation()
+        return false
+    }
 
     handleMouseUp = () => {
-        this.setState({ previewStickerId: 0, timestamp: 0, showPreview: false });
+        this.setState({ previewStickerId: 0, timestamp: 0, showPreview: false })
 
-        const { onPreview } = this.props;
+        const { onPreview } = this.props
 
-        onPreview(null);
+        onPreview(null)
 
-        this.mouseDown = false;
-        document.removeEventListener('mouseup', this.handleMouseUp);
-    };
+        this.mouseDown = false
+        document.removeEventListener('mouseup', this.handleMouseUp)
+    }
 
     handleStickerSelect = sticker => {
-        const { onSelect } = this.props;
-        const { cancelSend } = this.state;
+        const { onSelect } = this.props
+        const { cancelSend } = this.state
 
-        if (cancelSend) return;
+        if (cancelSend) return
 
-        onSelect(sticker);
-    };
+        onSelect(sticker)
+    }
 
     handleSelectSet = async position => {
-        const { sets, stickerSets } = this.state;
-        const { scrollRef } = this;
+        const { sets, stickerSets } = this.state
+        const { scrollRef } = this
 
         if (position < sets.length) {
-            const element = this.itemsMap.get(sets[position].id);
+            const element = this.itemsMap.get(sets[position].id)
             if (element) {
-                const node = ReactDOM.findDOMNode(element);
+                const node = ReactDOM.findDOMNode(element)
                 if (node) {
-                    const scroll = scrollRef.current;
-                    scroll.scrollTop = node.offsetTop;
+                    const scroll = scrollRef
+                    scroll.scrollTop = node.offsetTop
                 }
             }
         } else if (position < stickerSets.sets.length) {
-            if (this.loadingChunk) return;
-            if (stickerSets.sets.length === sets.length) return;
+            if (this.loadingChunk) return
+            if (stickerSets.sets.length === sets.length) return
 
-            this.loadingChunk = true;
-            const promises = [];
+            this.loadingChunk = true
+            const promises = []
             stickerSets.sets.slice(this.setsLength, position + 1).forEach(x => {
                 promises.push(
                     TdLibController.send({
                         '@type': 'getStickerSet',
-                        set_id: x.id
+                        set_id: x.id,
                     })
-                );
-            });
+                )
+            })
 
-            const result = await Promise.all(promises).finally(() => (this.loadingChunk = false));
+            const result = await Promise.all(promises).finally(() => (this.loadingChunk = false))
 
-            this.setsLength += result.length;
-            let concatSets = sets.concat(result);
+            this.setsLength += result.length
+            let concatSets = sets.concat(result)
             this.setState({ sets: concatSets }, () => {
                 if (position < concatSets.length) {
-                    this.handleSelectSet(position);
+                    this.handleSelectSet(position)
                 }
-            });
+            })
         }
-    };
+    }
+
+    onScrollRef = scrollRef => {
+        this.scrollRef = scrollRef
+    }
 
     render() {
-        const { position, stickerSets, sets, headerStickers } = this.state;
-        if (!stickerSets) return null;
+        const { position, stickerSets, sets, headerStickers } = this.state
+        if (!stickerSets) return null
 
-        if (!sets) return null;
-        if (!sets.length) return null;
+        if (!sets) return null
+        if (!sets.length) return null
 
-        this.itemsMap.clear();
+        this.itemsMap.clear()
         const items = sets.map(x => (
             <StickerSet
                 key={x.id}
@@ -358,22 +368,25 @@ class StickersPicker extends React.Component {
                 onMouseDown={this.handleMouseDown}
                 onMouseOver={this.handleMouseOver}
             />
-        ));
+        ))
 
         return (
             <div className='stickers-picker'>
                 <StickersPickerHeader onSelect={this.handleSelectSet} stickers={headerStickers} />
-                <div ref={this.scrollRef} className='stickers-picker-scroll' onScroll={this.handleScroll}>
+                <Scrollbar
+                    containerRef={this.onScrollRef}
+                    className='stickers-picker-scroll'
+                    onScrollY={this.handleScroll}>
                     {items}
-                </div>
+                </Scrollbar>
             </div>
-        );
+        )
     }
 }
 
 StickersPicker.propTypes = {
     onSelect: PropTypes.func.isRequired,
-    onPreview: PropTypes.func.isRequired
-};
+    onPreview: PropTypes.func.isRequired,
+}
 
-export default StickersPicker;
+export default StickersPicker
