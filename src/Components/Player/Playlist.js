567bc0324b90694b7420a64dbd1de3a3d8a1a829
Kesha Antonov <innokenty.longway@gmail.com>
2019-06-21 14:34:18 +0300

    In header player: when listening to voice message - hover over player - playlist will popup. Scrollbars not styled. | Done: added styled scrollbar.

--- src/Components/Player/Playlist.js
+++ src/Components/Player/Playlist.js
@@ -5,223 +5,237 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-import classNames from 'classnames';
-import { getMedia, openMedia } from '../../Utils/Message';
-import { borderStyle } from '../Theme';
-import withStyles from '@material-ui/core/styles/withStyles';
-import PlayerStore from '../../Stores/PlayerStore';
-import TdLibController from '../../Controllers/TdLibController';
-import './Playlist.css';
+import React from 'react'
+import * as ReactDOM from 'react-dom'
+import classNames from 'classnames'
+import { getMedia, openMedia } from '../../Utils/Message'
+import { borderStyle } from '../Theme'
+import withStyles from '@material-ui/core/styles/withStyles'
+import PlayerStore from '../../Stores/PlayerStore'
+import TdLibController from '../../Controllers/TdLibController'
+import Scrollbar from '../Scrollbar'
+import './Playlist.css'
 
 const styles = theme => ({
     root: {
-        background: theme.palette.type === 'dark' ? theme.palette.background.default : '#FFFFFF'
+        background: theme.palette.type === 'dark' ? theme.palette.background.default : '#FFFFFF',
     },
-    ...borderStyle(theme)
-});
+    ...borderStyle(theme),
+})
 
 class Playlist extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
 
-        this.listRef = React.createRef();
-        this.itemRefMap = new Map();
+        this.listRef = null
+        this.itemRefMap = new Map()
 
-        const { message, playlist } = PlayerStore;
+        const { message, playlist } = PlayerStore
 
-        this.chatId = message ? message.chat_id : 0;
-        this.messageId = message ? message.id : 0;
+        this.chatId = message ? message.chat_id : 0
+        this.messageId = message ? message.id : 0
 
         this.state = {
             open: false,
             titleMouseOver: false,
             playlistMouseOver: false,
-            playlist: playlist
-        };
+            playlist: playlist,
+        }
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        const { open } = this.state;
+        const { open } = this.state
 
         if (!prevState.open && open) {
-            this.scrollToActive();
+            this.scrollToActive()
         }
     }
 
     scrollToActive = () => {
-        const list = this.listRef.current;
-        if (!list) return;
+        const list = this.listRef
+        if (!list) return
 
-        const { messageId } = this;
-        if (!messageId) return;
+        const { messageId } = this
+        if (!messageId) return
 
-        const item = this.itemRefMap.get(messageId);
-        if (!item) return;
+        const item = this.itemRefMap.get(messageId)
+        if (!item) return
 
-        const node = ReactDOM.findDOMNode(item);
-        if (!node) return;
+        const node = ReactDOM.findDOMNode(item)
+        if (!node) return
 
-        list.scrollTop = node.offsetTop - 15;
-    };
+        list.scrollTop = node.offsetTop - 15
+    }
 
     componentDidMount() {
-        PlayerStore.on('clientUpdateMediaActive', this.onClientUpdateMediaActive);
-        PlayerStore.on('clientUpdateMediaPlaylist', this.onClientUpdateMediaPlaylist);
-        PlayerStore.on('clientUpdateMediaPlaylistLoading', this.onClientUpdateMediaPlaylistLoading);
-        PlayerStore.on('clientUpdateMediaTitleMouseOver', this.onClientUpdateMediaTitleMouseOver);
+        PlayerStore.on('clientUpdateMediaActive', this.onClientUpdateMediaActive)
+        PlayerStore.on('clientUpdateMediaPlaylist', this.onClientUpdateMediaPlaylist)
+        PlayerStore.on('clientUpdateMediaPlaylistLoading', this.onClientUpdateMediaPlaylistLoading)
+        PlayerStore.on('clientUpdateMediaTitleMouseOver', this.onClientUpdateMediaTitleMouseOver)
     }
 
     componentWillUnmount() {
-        PlayerStore.removeListener('clientUpdateMediaActive', this.onClientUpdateMediaActive);
-        PlayerStore.removeListener('clientUpdateMediaPlaylist', this.onClientUpdateMediaPlaylist);
-        PlayerStore.removeListener('clientUpdateMediaPlaylistLoading', this.onClientUpdateMediaPlaylistLoading);
-        PlayerStore.removeListener('clientUpdateMediaTitleMouseOver', this.onClientUpdateMediaTitleMouseOver);
+        PlayerStore.removeListener('clientUpdateMediaActive', this.onClientUpdateMediaActive)
+        PlayerStore.removeListener('clientUpdateMediaPlaylist', this.onClientUpdateMediaPlaylist)
+        PlayerStore.removeListener(
+            'clientUpdateMediaPlaylistLoading',
+            this.onClientUpdateMediaPlaylistLoading
+        )
+        PlayerStore.removeListener(
+            'clientUpdateMediaTitleMouseOver',
+            this.onClientUpdateMediaTitleMouseOver
+        )
     }
 
     onClientUpdateMediaTitleMouseOver = update => {
-        const { over } = update;
+        const { over } = update
 
         if (over) {
             this.setState(
                 {
-                    playlistMouseOver: over
+                    playlistMouseOver: over,
                 },
                 () => {
-                    this.tryOpen();
+                    this.tryOpen()
                 }
-            );
+            )
         } else {
             this.setState(
                 {
-                    playlistMouseOver: over
+                    playlistMouseOver: over,
                 },
                 () => {
-                    this.tryClose();
+                    this.tryClose()
                 }
-            );
+            )
         }
-    };
+    }
 
     onClientUpdateMediaActive = update => {
-        const { chatId, messageId } = update;
+        const { chatId, messageId } = update
 
-        this.chatId = chatId;
-        this.messageId = messageId;
-    };
+        this.chatId = chatId
+        this.messageId = messageId
+    }
 
     onClientUpdateMediaPlaylistLoading = update => {
-        const { chatId, messageId } = this;
+        const { chatId, messageId } = this
 
         if (update.chatId === chatId && update.messageId === messageId) {
             this.setState({
-                playlist: null
-            });
+                playlist: null,
+            })
         }
-    };
+    }
 
     onClientUpdateMediaPlaylist = update => {
-        const { chatId, messageId } = this;
-        const { playlist } = update;
+        const { chatId, messageId } = this
+        const { playlist } = update
 
         if (chatId === playlist.chatId && messageId === playlist.messageId) {
             this.setState({
-                playlist: playlist
-            });
+                playlist: playlist,
+            })
         }
-    };
+    }
 
     tryOpen = () => {
-        clearTimeout(this.openTimeout);
+        clearTimeout(this.openTimeout)
 
         this.openTimeout = setTimeout(() => {
-            const { titleMouseOver, playlistMouseOver } = this.state;
+            const { titleMouseOver, playlistMouseOver } = this.state
             this.setState({
-                open: titleMouseOver || playlistMouseOver
-            });
-        }, 250);
-    };
+                open: titleMouseOver || playlistMouseOver,
+            })
+        }, 250)
+    }
 
     tryClose = () => {
-        clearTimeout(this.timeout);
+        clearTimeout(this.timeout)
 
         this.timeout = setTimeout(() => {
-            const { titleMouseOver, playlistMouseOver } = this.state;
+            const { titleMouseOver, playlistMouseOver } = this.state
             this.setState({
-                open: titleMouseOver || playlistMouseOver
-            });
-        }, 250);
-    };
+                open: titleMouseOver || playlistMouseOver,
+            })
+        }, 250)
+    }
 
     handleMouseEnter = () => {
         this.setState({
             playlistMouseOver: true,
-            open: true
-        });
-    };
+            open: true,
+        })
+    }
 
     handleMouseLeave = () => {
         this.setState(
             {
-                playlistMouseOver: false
+                playlistMouseOver: false,
             },
             () => {
-                this.tryClose();
+                this.tryClose()
             }
-        );
-    };
+        )
+    }
 
     handleScroll = () => {
-        const list = this.listRef.current;
-        if (!list) return;
+        const list = this.listRef
+        if (!list) return
 
         if (list.scrollTop === 0) {
             TdLibController.clientUpdate({
-                '@type': 'clientUpdateMediaPlaylistNext'
-            });
+                '@type': 'clientUpdateMediaPlaylistNext',
+            })
         } else if (list.scrollHeight === list.scrollTop + list.offsetHeight) {
             TdLibController.clientUpdate({
-                '@type': 'clientUpdateMediaPlaylistPrev'
-            });
+                '@type': 'clientUpdateMediaPlaylistPrev',
+            })
         }
-    };
+    }
+
+    onListRef = listRef => {
+        this.listRef = listRef
+    }
 
     render() {
-        const { classes } = this.props;
+        const { classes } = this.props
 
-        const { open, playlist } = this.state;
-        if (!open) return null;
-        if (!playlist) return null;
+        const { open, playlist } = this.state
+        if (!open) return null
+        if (!playlist) return null
 
-        const { messages } = playlist;
-        if (!messages) return null;
-        if (messages.length <= 1) return null;
+        const { messages } = playlist
+        if (!messages) return null
+        if (messages.length <= 1) return null
 
-        this.itemRefMap.clear();
+        this.itemRefMap.clear()
 
         return (
             <div className='playlist'>
                 <div className={classNames('playlist-wrapper', classes.root, classes.borderColor)}>
-                    <div
-                        ref={this.listRef}
-                        className='playlist-items'
-                        onMouseEnter={this.handleMouseEnter}
-                        onMouseLeave={this.handleMouseLeave}
-                        onScroll={this.handleScroll}>
-                        {playlist.messages
-                            .slice(0)
-                            .reverse()
-                            .map(x => (
-                                <div key={x.id} ref={el => this.itemRefMap.set(x.id, el)} className='playlist-item'>
-                                    {getMedia(x, () => openMedia(x.chat_id, x.id))}
-                                </div>
-                            ))}
-                    </div>
+                    <Scrollbar containerRef={this.onListRef} onScrollY={this.handleScroll}>
+                        <div
+                            className='playlist-items'
+                            onMouseEnter={this.handleMouseEnter}
+                            onMouseLeave={this.handleMouseLeave}>
+                            {playlist.messages
+                                .slice(0)
+                                .reverse()
+                                .map(x => (
+                                    <div
+                                        key={x.id}
+                                        ref={el => this.itemRefMap.set(x.id, el)}
+                                        className='playlist-item'>
+                                        {getMedia(x, () => openMedia(x.chat_id, x.id))}
+                                    </div>
+                                ))}
+                        </div>
+                    </Scrollbar>
                 </div>
             </div>
-        );
+        )
     }
 }
 
-export default withStyles(styles)(Playlist);
+export default withStyles(styles)(Playlist)
