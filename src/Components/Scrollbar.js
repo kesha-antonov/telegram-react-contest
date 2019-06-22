11904d8fea8f905a582791aecd566cb4be80afd4
Kesha Antonov <innokenty.longway@gmail.com>
2019-06-22 20:00:39 +0300

    scrollbar: fix sryles override

--- src/Components/Scrollbar.js
+++ src/Components/Scrollbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import 'react-perfect-scrollbar/dist/css/styles.css'
+import './Scrollbar.css'
 import PerfectScrollbar from 'react-perfect-scrollbar'
 import { connect } from 'react-redux'
 
