e6f72a4b420073f792e482e45f0aeef170f5bee9
Kesha Antonov <innokenty.longway@gmail.com>
2019-06-22 22:55:00 +0300

    bugfix. scrollbar: smooth scroll on touch devices

--- src/Components/Scrollbar.js
+++ src/Components/Scrollbar.js
@@ -6,7 +6,24 @@ import './Scrollbar.css'
 import PerfectScrollbar from 'react-perfect-scrollbar'
 import { connect } from 'react-redux'
 
+const isTouchDevice =
+    'ontouchstart' in document.documentElement ||
+    'ontouchstart' in window ||
+    window.navigator.msPointerEnabled
+
 function Scrollbar(props) {
+    if (isTouchDevice) {
+        return (
+            <div
+                {...props}
+                className={classNames(props.className, 'scrollbar-touch')}
+                ref={props.containerRef}
+                onScroll={props.onScrollY}>
+                {props.children}
+            </div>
+        )
+    }
+
     const isLightTheme = props.palette.type === 'light'
 
     return (
@@ -27,6 +44,8 @@ function Scrollbar(props) {
 Scrollbar.propTypes = {
     palette: PropTypes.object.isRequired,
     children: PropTypes.any,
+    containerRef: PropTypes.func,
+    onScrollY: PropTypes.func,
 }
 
 const mapStateToProps = state => {
